53d07c239e67988c44401b26185425b7f30d4023
Ryan Roemer <ryan@loose-bits.com>
2017-03-19 13:13:59 -0700

    Start config stuff.

--- bin/lank.js
+++ bin/lank.js
@@ -2,5 +2,9 @@
 "use strict";
 
 const opts = require("../lib/args").parse();
+const config = require("../lib/config").config();
 
-console.log("lank options: ", JSON.stringify(opts, null, 2)); // eslint-disable-line
+console.log("lank setup: ", JSON.stringify({ // eslint-disable-line
+  opts,
+  config
+}, null, 2)); // eslint-disable-line
