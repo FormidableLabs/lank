e40f4ed91f0bb66a0ec5ffd312d47680e95b1e22
Ryan Roemer <ryan@loose-bits.com>
2017-04-18 12:32:51 -0700

    Add modules filter.

--- bin/lank.js
+++ bin/lank.js
@@ -8,7 +8,6 @@ const getConfig = require("../lib/config").getConfig;
 
 const util = require("../lib/util");
 const getFmt = util.getFmt;
-const filterTags = util.filterTags;
 let fmt = (args) => JSON.stringify(args);
 
 const ARGS_IDX = 2; // index where cli arguments start
@@ -16,6 +15,9 @@ const ARGS_IDX = 2; // index where cli arguments start
 const main = module.exports = (argv) => {
   const args = parse(argv) || {};
   const action = args.action || (() => {});
+  const tags = args.tags || [];
+  const mods = args.mods || [];
+
   let cfg;
 
   // Get configuration.
@@ -23,10 +25,17 @@ const main = module.exports = (argv) => {
     // Stash config, run action.
     .then((rawCfg) => {
       // Process and stash configs + formatter.
-      cfg = (rawCfg || []).filter(filterTags(args.tags));
+      cfg = (rawCfg || [])
+        // Filter to tags.
+        .filter((obj) => !tags.length || obj.tags.some((tag) => tags.indexOf(tag) > -1))
+        // Filter to modules.
+        .filter((obj) => !mods.length || mods.indexOf(obj.module) > -1);
+
       fmt = getFmt(cfg);
       if (!cfg.length) {
-        throw new Error(`Found no matching projects. Tags: '${args.tags.join("', '")}'`);
+        const tagsMsg = `Tags: '${tags.join("', '")}'.`;
+        const modsMsg = `Modules: '${mods.join("', '")}'.`;
+        throw new Error(`Found no matching projects. ${tagsMsg} ${modsMsg}`);
       }
 
       return action(cfg, args);
